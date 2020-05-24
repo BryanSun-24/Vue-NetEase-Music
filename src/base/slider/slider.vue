e43c7a97d7cf2767474ad0467227f7a60e002cf5
Kai Sun <bryant.kai1227@gmail.com>
2020-05-24 17:39:06 -0400

    add api

---
+++ src/base/slider/slider.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="slider"></div>
+</template>
+<script>
+import {getBanner} from "../../api/recommend"
+import {ERR_OK} from "../../common/js/config"
+export default {
+    name:"slider",
+    data(){
+        return {
+            bannerList: []
+        }
+    },
+    created(){
+        this._getBanner();
+    },
+    methods:{
+        _getBanner(){
+            getBanner().then((res) => {
+                if (res.status == ERR_OK){
+                    this.bannerList = res.data.banners;
+                }
+            }) 
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
